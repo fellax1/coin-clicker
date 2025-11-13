006e334d0003e51e7b229aded7a498ec26337a9c
Petter Astedt <petter.astedt@bonniernews.se>
2025-11-13 17:36:40 +0100

    Move courses to separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,8 @@
 import { useEffect, useState } from "react";
+import { courses } from "./courses";
 import "./App.css";
 
 const MAX_INTERNS = 5;
-
-const courses = [
-  {
-    id: 1,
-    name: "Efficiency Workshop #1",
-    cost: 500,
-    multiplierIncrease: 1.1,
-    description: "Increases income per click by 10%"
-  },
-  {
-    id: 2,
-    name: "Productivity Bootcamp",
-    cost: 1500,
-    multiplierIncrease: 2,
-    description: "Increases income per click by 100%"
-  },
-  {
-    id: 3,
-    name: "Leadership Masterclass",
-    cost: 5000,
-    multiplierIncrease: 10,
-    description: "Increases income per click by 900%"
-  },
-  {
-    id: 4,
-    name: "Innovation Workshop",
-    cost: 15000,
-    multiplierIncrease: 15,
-    description: "Increases income per click by 1400%"
-  },
-  {
-    id: 5,
-    name: "Executive Training",
-    cost: 50000,
-    multiplierIncrease: 25,
-    description: "Increases income per click by 2400%"
-  }
-];
 
 const intern = {
   type: "intern",
